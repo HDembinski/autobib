0c8ed6f1ede10712cb32e515e9b15274e1c2473a
Hans Dembinski <hans.dembinski@gmail.com>
2021-05-05 13:23:48 +0200

    fix

--- tests/test_autobib.py
+++ tests/test_autobib.py
@@ -1,11 +1,15 @@
 import subprocess as subp
 import shutil
 from pathlib import Path
+import pytest
 
 cwd = Path(__file__).parent
 test_document_path = cwd / "data"
 
 
+@pytest.mark.skipif(
+    subp.run(["latex", "--version"]).returncode != 0, reason="requires latex"
+)
 def test_autobib(tmpdir):
     shutil.copy(test_document_path / "main.tex", tmpdir / "main.tex")
 
